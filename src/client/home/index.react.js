37969f0bc6031442314ead8ef2b9c60b452edb79
Petr Messner <petr.messner@gmail.com>
2015-09-09 00:51:58 +0200

    sample data hardcoded

--- src/client/home/index.react.js
+++ src/client/home/index.react.js
@@ -17,8 +17,32 @@ export default class HomeIndex extends Component {
       <DocumentTitle title={msg.title}>
         <div className="home-page">
 
-          
+          <table>
 
+            <tr>
+              <td>Středa 9. 9. 2015</td>
+              <td><strong>Praha</strong></td>
+              <td>MLMU</td>
+              <td>
+                <span className="event-name">Using Apache Spark (Peter Zvirinský)</span>
+                <span className="event-name-tags-separator">&middot;</span>
+                <span className="event-tag">Apache Spark</span>
+                <span className="event-tag">Hadoop</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Středa 24. 9. 2015</td>
+              <td><strong>Brno</strong></td>
+              <td>Brněnské Pyvo</td>
+              <td>
+                <span className="event-name">3.5 Release Party</span>
+                <span className="event-name-tags-separator">&middot;</span>
+                <span className="event-tag">Python</span>
+              </td>
+            </tr>
+
+          </table>
 
         </div>
       </DocumentTitle>
